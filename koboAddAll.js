40b30f548ee337d0bbc12b1773d6117e347b65d9
toki <toki.kanno@gmail.com>
2018-09-09 02:55:42 +0800

    update

--- koboAddAll.js
+++ koboAddAll.js
@@ -15,7 +15,7 @@ function getKoboApiUrl(apiPath) {
 }
 
 function addToCart(pid, apiUrl='/zh/tw/shoppingcartwidget/add') {
-    $.post(
+    return $.post(
         apiUrl, 
         {
             Id: pid,
@@ -25,22 +25,44 @@ function addToCart(pid, apiUrl='/zh/tw/shoppingcartwidget/add') {
         'json'
     ).done((resp) => {
         console.log(resp);
+
+        const elm = $('#kobo-aa-dimmer .main .inner');
+        if (elm.length == 0) {
+            return;
+        }
+
+        elm.append(`<div class="kobo-aa result-items">${resp.ItemDetails.Title} - ${resp.IsSuccess ? '已成功加入購物車' : resp.ErrorMessage}</div>`);
     });
 }
 
-function koboAddAll() {
+function koboAddAll(jq=$) {
 
     const apiUrl = getKoboApiUrl('/shoppingcartwidget/add');
-    console.log(apiUrl);
+    const reqList = [];
 
     $.each(
-        $('li.item-wrapper.book'),
+        jq.find('li.item-wrapper.book'),
         (idx, x) => {
             const pid = $(x).data('track-info').productId;
-            console.log(pid);
-            addToCart(pid, apiUrl);
+            reqList.push(
+                addToCart(pid, apiUrl)
+            );
         }
     );
+
+    return $.when(reqList);
+}
+
+function koboFindNextPage(jq=$) {
+    if (jq.find('div.pagination').length == 0) {
+        return '';
+    }
+
+    if (jq.find('div.pagination .next.disabled').length > 0) {
+        return '';
+    }
+
+    return jq.find('div.pagination a.page-link.active').next().attr('href');
 }
 
 function addSequenceShortCuts(){
@@ -52,5 +74,70 @@ function addSequenceShortCuts(){
     );
 }
 
-console.log('koboAddAll loaded ...');
+function addDimmer() {
+    if ($('#kobo-aa-dimmer').length > 0) {
+        return;
+    }
+
+    let dimmerElm = $(`
+<div id="kobo-aa-dimmer">
+    <div class="main">
+        <div class="inner"></div>
+    </div>
+</div>`);
+    $('body').append(dimmerElm).addClass('kobo-aa-dimmer');
+}
+
+function dismissDimmer() {
+    $('#kobo-aa-dimmer').remove();
+    $('body').removeClass('kobo-aa-dimmer');
+}
+
+function koboAddAllAjax(jq) {
+    const apiUrl = getKoboApiUrl('/shoppingcartwidget/add');
+    const reqList = [];
+    $.each(
+        jq.filter('li'),
+        (idx, x) => {
+            const pid = $(x).data('track-info').productId;
+            reqList.push(
+                addToCart(pid, apiUrl)
+            );
+        }
+    )
+
+    return $.when(reqList);    
+}
+
+function processSeqPage(url, finalCB) {
+    $.get(url)
+    .done((resp) => {
+        const jq = $(resp.Items);
+        koboAddAllAjax(jq)
+        .then(() => {
+            console.log('koboAddAllAjax.done');
+            if (resp.HasMoreResults) {
+                if (/&pageNumber=(\d+)/.test(url)) {
+                    const page = parseInt(/&pageNumber=(\d+)/.exec(url)[1], 10);
+                    url.replace(/&pageNumber=(\d+)/, `&pageNumber=${page + 1}`);
+                } else {
+                    url += '&pageNumber=2';
+                }
+                processSeqPage(url);
+            }
+        })
+    })
+    .fail((jqXHR, textStatus, errorThrown) => {
+        alert(textStatus);
+        finalCB(false);
+    });
+}
+
 addSequenceShortCuts();
+
+// bind event
+$('div.kobo-aa.add-btn').on('click', (e)=>{
+    const initUrl = e.target.dataset.href;
+    addDimmer();
+    processSeqPage(initUrl, ()=>{alert('完成~~')});
+});
