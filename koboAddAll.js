bef5bf8dafa046b6fb86dfdd78ae72f0c99ba8a3
toki <toki.kanno@gmail.com>
2018-09-08 22:49:09 +0800

    script & css

---
+++ koboAddAll.js
@@ -0,0 +1,56 @@
+function getLocation(href) {
+    var l = document.createElement('a');
+    l.href = href;
+    return l;
+}
+
+function getKoboApiUrl(apiPath) {
+    const curLoc = getLocation(window.location.href);
+    const splits = curLoc.pathname.split('/');
+    if (splits.length < 3) {
+        return;
+    }
+
+    return '/' + [splits[1], splits[2], apiPath.replace(/^\//, '')].join('/');
+}
+
+function addToCart(pid, apiUrl='/zh/tw/shoppingcartwidget/add') {
+    $.post(
+        apiUrl, 
+        {
+            Id: pid,
+            IsKoboLoveMembership: false
+        },
+        null,
+        'json'
+    ).done((resp) => {
+        console.log(resp);
+    });
+}
+
+function koboAddAll() {
+
+    const apiUrl = getKoboApiUrl('/shoppingcartwidget/add');
+    console.log(apiUrl);
+
+    $.each(
+        $('li.item-wrapper.book'),
+        (idx, x) => {
+            const pid = $(x).data('track-info').productId;
+            console.log(pid);
+            addToCart(pid, apiUrl);
+        }
+    );
+}
+
+function addSequenceShortCuts(){
+    $.each(
+        $('span.product-sequence-field'),
+        (idx, x) => {
+            $(x).append('<a class="kobo-aa add-btn" href="#">test</a>');
+        }
+    );
+}
+
+console.log('koboAddAll loaded ...');
+addSequenceShortCuts();
